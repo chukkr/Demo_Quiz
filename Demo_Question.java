ef32290e00c8c11a2b3b7ad6265019748a15dfe9
EliteFire <63857639+EliteFire@users.noreply.github.com>
2020-06-10 19:48:41 +1000

    Update Demo_Question.java

--- Demo_Question.java
+++ Demo_Question.java
@@ -1,68 +1,69 @@
-/**********************************************************/
 // Filename:  Demo_Question.java
 // Purpose:   Framework of question and used to create the question that is used
 //            by both student and teacher GUI (Demo Only)
-// Author:    Rik Reyes
-// Version:   1.0
+// Author:    Rik Reyes & Christopher Morley
+// Version:   1.1
+// Notes:     - Added implements Comparable<Demo_Question> 
+//            - Added toString() method
+//            - Added compareTo () method
 // Date:	   10-June-2020	   
 /**********************************************************/
 package mathquiz;
 
-public class Demo_Question 
+public class Demo_Question implements Comparable<Demo_Question>
 {
+    
     private int answer;
-
     private int leftOperand;
-
     private String operator;
-
     private int rightOperand;
     
-    
-     public Demo_Question (int lOp, String op, int rOp, int ans)
+    public Demo_Question (int lOp, String op, int rOp, int ans)
     {
-
         answer = ans;
-
         leftOperand = lOp;
-
         operator = op;
-
         rightOperand = rOp; 
-
     }
-
     
-
     public int getLOp()
-
     {
-
         return leftOperand;
-
     }
-
     public int getROp()
-
     {
-
         return rightOperand;
-
     }
-
     public String getOperator()
-
     {
-
         return operator;
-
     }
-
     public int getAnswer()
-
     {
-
         return answer;
-
     }
+
+    @Override
+    public String toString()
+    {
+        //Question format
+        return answer + "(" + leftOperand + " " + operator + " " + rightOperand + ")";
+    }
+
+    // return integer values 0, -1 or 1
+    public int compareTo (Demo_Question anotherQuiz)
+    {
+        int comparison = 0;
+        if (this.answer < anotherQuiz.answer)
+        {
+            comparison = -1;
+        }
+        else if (this.answer > anotherQuiz.answer)
+        {
+            comparison = 1;
+        }
+
+        return comparison;
+    }
+
 }
